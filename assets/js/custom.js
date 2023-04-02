b10d9d41fe8dc6bd0224df3af8593ebc1e9c1793
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-04-02 16:46:32 +0200

    removed loader

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -185,13 +185,13 @@
 			});
 		}
 
-		$("#preloader").animate({
+	/*	$("#preloader").animate({
 			'opacity': '0'
 		}, 600, function(){
 			setTimeout(function(){
 				$("#preloader").css("visibility", "hidden").fadeOut();
 			}, 300);
-		});
+		});*/
 	});
 
 
